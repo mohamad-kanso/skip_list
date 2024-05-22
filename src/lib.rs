1985ed23bba6a9f3b87f8484f35fb992fdfd8fac
mohamadk0234 <kansomohamad@yahoo.com>
2024-05-22 14:11:14 +0300

    safe implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
-use std::{fmt::Display, ptr::NonNull};
+use std::{cell::RefCell, fmt::Display, rc::Rc};
 
 use rand::Rng;
 
-#[derive(Clone,Debug)]
+#[derive(Clone, Default, Debug, PartialEq)]
 struct Node {
    key: i32,
-   next: Vec<Option<NonNull<Node>>>
+   next: Vec<Option<Link>>
 }
+
+type Link = Rc<RefCell<Node>>;
 
 impl Node {
    fn new(key: i32, max_level: usize) -> Self{
@@ -14,23 +16,11 @@ impl Node {
    }
 }
 
+#[derive(Debug)]
 pub struct SkipList {
-   head: NonNull<Node>,
+   head: Link,
+   level: usize,
    max_level: usize
-}
-
-type Link = Option<NonNull<Node>>;
-
-fn value (node: NonNull<Node>) -> Option<i32> {
-   unsafe { Some((*node.as_ptr()).key.clone())}
-}
-
-fn forward (node: NonNull<Node>) -> Vec<Link> {
-   unsafe { (*node.as_ptr()).next.clone()}
-}
-
-fn forward_mut (link: &mut NonNull<Node>) -> &mut [Link]{
-   unsafe { &mut (*link.as_ptr()).next }
 }
 
 impl Display for SkipList {
@@ -41,9 +31,9 @@ impl Display for SkipList {
          if i == 0 {
             row.push(0);
          }
-         let mut node = forward(self.head)[i];
-         while let Some(v) = node.and_then(value) {
-            node = forward(node.unwrap())[i];
+         let mut node = self.head.borrow().next[i].clone();
+         while let Some(v) = Some(node.clone().unwrap().borrow().key){
+            node = node.unwrap().as_ref().borrow().next[i].clone();
 
             if i == 0 {row.push(v)}
             else {
@@ -84,86 +74,153 @@ impl Display for SkipList {
 
 impl SkipList {
    pub fn new (max_level:usize) -> Self {
-      let node = Box::leak(
-         Box::new(Node::new(0,max_level))
-      ).into();
-      Self { head: node, max_level}
+      let node: Node = Node::new(0,max_level);
+      Self { head: Rc::new(RefCell::new(node)), level:0, max_level}
    }
 
    pub fn search(&self, k:i32) -> Option<i32>{
-      let mut node = self.head;
+      let mut node = self.head.clone();
       for l in (0..self.max_level).rev(){
-         unsafe{
-            while let Some(next) = (*node.as_ptr()).next[l]{
-               let key = next.as_ref().key;
-               if key == k {
-                  return Some(next.as_ref().key)
-               }
-               if key < k {
-                  node = next;
-               }
-               else {break;}
+        while let Some(next) = node.clone().borrow_mut().next[l].clone(){
+            let key = next.borrow().key;
+            if key == k {
+               return Some(next.borrow().key);
             }
-         }
+            if key < k {
+               node = next;
+            }
+            else {break;}
+        }
       }
       None
    }
 
-   pub fn insert(&mut self, k: i32) -> Option<i32> {
-      let mut node = self.head;
-      let mut updates = vec![None;self.max_level];
-      for l in (0..self.max_level).rev() {
-         unsafe {
-            while let Some(next) = node.as_ref().next[l]{
-               let key = next.as_ref().key;
-               if key == k {
-                  println!("key already in list");
-                  return Some(k);
-               }
-               if key < k {
-                  node = next;
-               } else {break}
+   fn random_level(&self) -> usize{
+        let mut level = 0;
+        let mut rng = rand::thread_rng();
+        while rng.gen::<f32>() < 0.5 && level < self.max_level {
+            level += 1;
+        }
+        if level < self.max_level {return level}
+        else {return level - 1}
+   }
+
+   pub fn insert(&mut self, key: i32) {
+        let random_level = self.random_level();
+        
+        let update = self.fill_update_vector(&self.head, vec![None; random_level+1], &key, random_level);
+
+        if let Some(node) = &update[0] {
+            if let Some(next_node) = node.borrow().next[0].as_ref() {
+                if next_node.borrow().key == key {
+                    println!("Item {:?} already inserted", key);
+                    return
+                }
+            }
+        }
+
+        let new_node = Rc::new(RefCell::new(
+            Node { 
+               key, 
+               next: vec![None; random_level+1]
+            }
+        ));
+        
+        for level in 0..=random_level {
+            let node = update[level].as_ref().unwrap();
+
+            let mut new_node_inner = new_node.take();
+            let mut node_inner = node.take();
+            
+            new_node_inner.next[level] = node_inner.next[level].take();
+            new_node.replace(new_node_inner);
+
+            node_inner.next[level] = Some(Rc::clone(&new_node));
+            node.replace(node_inner);
+        }
+
+        if random_level > self.level {
+            self.level = random_level
+        }
+    }
+
+    fn fill_update_vector(&self, 
+        cursor: &Link, 
+        mut update: Vec<Option<Link>>, 
+        key: &i32, 
+        level: usize) -> Vec<Option<Link>> {
+
+        if let Some(node) = &cursor.borrow().next[level] {
+            if node.borrow().key < *key {
+                return self.fill_update_vector(node, update, key, level);
+            }
+        }
+
+        update[level] = Some(Rc::clone(cursor));
+
+        if level > 0 {
+            return self.fill_update_vector(cursor, update, key, level-1);
+        }
+        update
+    }
+    pub fn delete(&mut self, key: i32) {
+      let update = self.fill_update_vector(&self.head, vec![None; self.level+1], &key, self.level);
+
+      let mut option_delete = None;
+      if let Some(update_node) = update[0].as_ref() {
+         if let Some(next_node) = update_node.borrow().next[0].as_ref() {
+            if next_node.borrow().key == key {
+               option_delete = Some(Rc::clone(next_node));
             }
          }
-         updates[l] = Some(node);
       }
-      let level = rand::thread_rng().gen_range(0..self.max_level);
-      let mut x: NonNull<Node> =  Box::leak(Box::new(Node::new(k, self.max_level))).into();
-      for i in 0..=level {
-         if updates[i].is_none() {
-            forward_mut(&mut x)[i] = forward(self.head)[i];
-            forward_mut(&mut self.head)[i] = Some(x);
-         }
-         else {
-            forward_mut(&mut x)[i] = forward(updates[i].unwrap())[i];
-            forward_mut(&mut updates[i].unwrap())[i] = Some(x); 
-         }
-      }
-      None
-   }
+      
+      if let Some(delete_node) = option_delete {
+          for level in 0..=self.level {                
 
-   pub fn delete (&mut self,k: i32) {
-      let mut update = vec![None;self.max_level];
-      let mut node = self.head;
-      for i in (0..self.max_level).rev() {
-         let mut next = forward(node)[i];
-         while next.and_then(value).is_some_and(|key| key < k){
-            node = next.unwrap();
-            next = forward(node)[i];
-         }
-         update[i]=Some(node);
-      }
+              let cursor = update[level].as_ref().unwrap();
+              if cursor.borrow().next[level].is_none() {
+                  break;
+              }
+              else if !Rc::ptr_eq(
+                  cursor.borrow().next[level].as_ref().unwrap(), 
+                  &delete_node) {
+                  break;
+              }
 
-      let node = forward(node)[0];
-      if node.map(value).is_some_and(|n| n.as_ref() == Some(&k)){
-         for i in 0..self.max_level {
-            if let Some(mut update_i) = update[i] {
-               if forward(update_i)[i] != node {break}
-               else {
-                  forward_mut(&mut update_i)[i] = forward(node.unwrap())[i];
-               }
-            } 
-         }
+              let mut delete_node_inner = delete_node.take();
+              let mut node_inner = cursor.take();
+              node_inner.next[level] = delete_node_inner.next[level].take();
+
+              cursor.replace(node_inner);
+              delete_node.replace(delete_node_inner);
+          }
+          while self.level > 1 && self.head.borrow().next[self.level].is_none() {
+              self.level -= 1;
+          }
       }
+      else {
+          println!("Item {:?} not found", key);
+      }
+  }
+
+  pub fn display(&self) {
+   if self.head.borrow().next[0].is_none() {
+       println!("Skiplist is empty.");
+       return
    }
+   self.display_recursive(&self.head, self.level);
+   println!();
+}
+
+fn display_recursive(&self, cursor: &Link, level: usize) {
+   if let Some(node) = cursor.borrow().next[level].as_ref() {
+       print!("[{:?}] -> ", node.borrow().key);
+       return self.display_recursive(&node, level);
+   }
+   println!();
+   if level != 0 {
+       return self.display_recursive(&self.head, level-1);
+   }
+}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use skip_list::SkipList;
 
 fn main(){
-    let mut ss = SkipList::new(5);
+    let mut ss = SkipList::new(3);
     ss.insert(2);
     ss.insert(3); 
     ss.insert(10);
     ss.insert(7);
     ss.insert(20);
     ss.insert(15);
-    println!("{}",ss);
+    ss.display();
     println!();
     ss.delete(7);
-    println!("{}",ss);
+    ss.display();
     match ss.search (7){
         Some(_) => println!("found key"),
         None => println!("key not found")
